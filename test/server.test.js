890f6c7ea2bd9662366cf7308e71704c21e1b607
Nathan P. Keene <npk@txtincorporated.com>
2016-10-29 20:38:11 -0700

    Placeholder submission

--- test/server.test.js
+++ test/server.test.js
@@ -1,25 +1,18 @@
 const assert = require('assert');
+const server = require('../lib/server');
+
 //Test server, router and request handlers
-describe('server preps and passes request data to router', (args1) => {
-  it('passes test 1', (args1) => {
-    let value1 = 1;
-    assert.equal(value1, 1);
-    done();
+describe('server preps and passes request data to router', () => {
+  it('passes test 1', () => {
   });
 });
 
-describe('router preps, passes route-specific server-side and req data, and function calls to handlers', (args2) => {
-  it('passes test 2', (args2) => {
-    let value2 = 1;
-    assert.equal(value2, 1);
+describe('router preps, passes route-specific server-side and req data, and function calls to handlers', () => {
+  it('passes test 2', () => {
   });
-  done();
 });
 
-describe('handlers process data correctly and complete server response to client', (args3) => {
-  it('passes test 3', (args3) => {
-    let value3 = 1;
-    assert.equal(value3, 1);
+describe('handlers process data correctly and complete server response to client', () => {
+  it('passes test 3', () => {
   });
-  done();
 });
